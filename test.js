517e7b59b5e7316b317e97d878bf488cb845e5c5
NexLFC <45148835+NexLFC@users.noreply.github.com>
2018-12-11 15:07:58 +0200

    remaking navbar to accommodate dropdown after break

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("content");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+function hiddenButton() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
 }
